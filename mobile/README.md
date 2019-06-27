71859e0bbdfeba3f3f41a174047fd9903edb884b
Sergio Abril (MBP) <sergio.abril@me.com>
2019-06-27 23:04:13 +0200

    added mobile scripts and go main for lnd-mobile

---
+++ mobile/README.md
@@ -0,0 +1,10 @@
+Original files:
+https://github.com/sergioabril/lnd/tree/mobile-build-tool
+
+Modified according Youtube talk: 
+https://www.youtube.com/watch?v=IIxCYrRiP78
+
+
+How To:
+
+./build_mobile.sh
---
+++ mobile/bindings.go
@@ -0,0 +1,38 @@
+package lndmobile
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	flags "github.com/jessevdk/go-flags"
+	"github.com/lightningnetwork/lnd"
+	"google.golang.org/grpc/test/bufconn"
+)
+
+var (
+	lightningLis		= bufconn.Listen(100)
+	walletUnlockerLis 	= bufconn.Listen(100)
+)
+
+func Start(extraArgs string, callback Callback) {
+	// Add the extra arguments to os.Args, as that will be parsed during
+	// startup
+	os.Args = append(os.Args, string.Fields(extraArgs)...)
+
+	// Call the "real" main in a nested manner so the defers will properly
+	// be exectured in the case of a graceful shutdown
+	go func() {
+		if err := lnd.Main(walletUnlockerLis, lightningLis); err != nil {
+			if e, ok := err.(*flags.Error); ok &&
+				e.Type == flags.ErrHelp {
+			} else {
+				fmt.Fprintln(os.Stderr, err)
+			}
+			os.Exit(1)
+		}
+	}()
+
+	// TODO(halseth): callback when RPC server is running.
+	callback.OnResponse([]byte("started"))
+}
