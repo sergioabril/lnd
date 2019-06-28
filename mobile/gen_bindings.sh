35e4b014222883fda8b3050462cecfe5d143a93b
Sergio Abril (MBP) <sergio.abril@me.com>
2019-06-28 20:04:20 +0200

    built a newer framework after generating bindings

--- mobile/gen_bindings.sh
+++ mobile/gen_bindings.sh
@@ -5,7 +5,7 @@ protoc -I/usr/local/include -I. \
        -I$GOPATH/src \
        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
        --plugin=protoc-gen-custom=$GOPATH/bin/promobile \
-       --custom_out=./build \
-       --custom_opt='package_name=lndmobile,target_package=github.com/lightningnetwork/lnd/lnrpc' \
+       --custom_out=. \
+       --custom_opt='listeners=[walletunlocker=bufwalletunlockerserv test=dos],package_name=lndmobile,target_package=github.com/lightningnetwork/lnd/lnrpc' \
        --proto_path=../lnrpc \
        rpc.proto
